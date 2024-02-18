b9f6e17809e151f5e9193963840b3e86a85155a4
montheooo <montheooo@yahoo.fr>
2024-02-18 13:03:02 -0500

    Create DTO for invoices controller

---
+++ Entities.DTO/FactureDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTO
+{
+    public class FactureDTO
+    {
+        public DateTime dateFacture { get; set; }
+        public string nomClient { get; set; }
+        public int numeroFacture { get; set; }
+        public string referenceFacture { get; set; }
+        public List<LigneFactureDTO> ligneFactures { get; set; } = new List<LigneFactureDTO>() ;
+
+
+    }
+}
---
+++ Entities.DTO/LigneFactureDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTO
+{
+    public class LigneFactureDTO
+    {
+        public string idArticle { get; set; }
+        public string nomArticle { get; set; }
+        public decimal prixUnitaire { get; set; }
+        public decimal quantite { get; set; }
+    }
+}
